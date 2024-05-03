c588a42bfc41c3016074f615f1ed6978a0854acf
Rfluid <ruy.vieiraneto@gmail.com>
2024-05-03 12:50:22 -0300

    Add contacts support.

--- src/message/model/content.model.go
+++ src/message/model/content.model.go
@@ -3,21 +3,23 @@ package message_model
 
 import (
 	media_model "github.com/Rfluid/whatsapp-cloud-api/src/media/model"
+	message_content_type_model "github.com/Rfluid/whatsapp-cloud-api/src/message/model/content-type"
 	message_type_common_model "github.com/Rfluid/whatsapp-cloud-api/src/message/model/content-type/common"
 )
 
 // The message real content seen by user.
 type Content struct {
-	Text        *message_type_common_model.Text     `json:"text,omitempty"`
-	Reaction    *message_type_common_model.Reaction `json:"reaction,omitempty"`
-	Image       *media_model.UseMedia               `json:"image,omitempty"`
-	Video       *media_model.UseMedia               `json:"video,omitempty"`
-	Document    *media_model.UseMedia               `json:"document,omitempty"`
-	Audio       *media_model.UseMedia               `json:"audio,omitempty"`
-	Sticker     *media_model.UseMedia               `json:"sticker,omitempty"`
-	Location    *message_type_common_model.Location `json:"location,omitempty"`
-	Interactive interface{}                         `json:"interactive,omitempty"`
-	Template    interface{}                         `json:"template,omitempty"`
+	Text        *message_type_common_model.Text       `json:"text,omitempty"`
+	Reaction    *message_type_common_model.Reaction   `json:"reaction,omitempty"`
+	Image       *media_model.UseMedia                 `json:"image,omitempty"`
+	Video       *media_model.UseMedia                 `json:"video,omitempty"`
+	Document    *media_model.UseMedia                 `json:"document,omitempty"`
+	Audio       *media_model.UseMedia                 `json:"audio,omitempty"`
+	Sticker     *media_model.UseMedia                 `json:"sticker,omitempty"`
+	Location    *message_type_common_model.Location   `json:"location,omitempty"`
+	Template    *message_content_type_model.Template  `json:"template,omitempty"`
+	Interactive interface{}                           `json:"interactive,omitempty"`
+	Contacts    *[]message_content_type_model.Contact `json:"contacts,omitempty"`
 }
 
 // TODO: Add validate content method.
