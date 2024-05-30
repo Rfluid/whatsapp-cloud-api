ade41a840f0c460480893d6850522638ebc1d2c1
Rfluid <ruy.vieiraneto@gmail.com>
2024-05-30 11:43:36 -0300

    Improve template models.

--- src/common/enum/enpoints.enum.go
+++ src/common/enum/enpoints.enum.go
@@ -13,4 +13,5 @@ const (
 	Register               Endpoint = "register"
 	DeRegister             Endpoint = "deregister"
 	Media                  Endpoint = "media"
+	Templates              Endpoint = "message_templates"
 )
