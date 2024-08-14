ae2a24c2592c78fb2b1ec4044def6ec4bd2a5d0d
Rfluid <ruy.vieiraneto@gmail.com>
2024-08-14 17:47:22 -0300

    Fix webhook contact

--- src/webhook/model/contact.model.go
+++ src/webhook/model/contact.model.go
@@ -2,6 +2,10 @@ package webhook_model
 
 // Contact object with information for the customer who sent a message to the business.
 type Contact struct {
-	WaId    string `json:"wa_id"`
-	Profile string `json:"profile"`
+	WaId    string         `json:"wa_id"`
+	Profile ContactProfile `json:"profile"`
+}
+
+type ContactProfile struct {
+	Name string `json:"name,omitempty"`
 }
