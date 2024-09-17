8bb908574aa202efe325d15646659e69d7cb262c
Rfluid <ruy.vieiraneto@gmail.com>
2024-09-16 21:28:59 -0300

    Fix interactive text

--- src/message/model/content-type/interactive/body.model.go
+++ src/message/model/content-type/interactive/body.model.go
@@ -1,10 +1,8 @@
 package message_type_interactive_model
 
-import message_type_common_model "github.com/Rfluid/whatsapp-cloud-api/src/message/model/content-type/common"
-
 // Described at
 //
 // https://developers.facebook.com/docs/whatsapp/on-premises/reference/messages#body-object
 type Body struct {
-	Text message_type_common_model.TextData `json:"text"` // Required if body is present
+	Text *string `json:"text,omitempty"` // Required if body is present
 }
--- src/message/model/content-type/interactive/header.model.go
+++ src/message/model/content-type/interactive/header.model.go
@@ -2,7 +2,6 @@ package message_type_interactive_model
 
 import (
 	media_model "github.com/Rfluid/whatsapp-cloud-api/src/media/model"
-	message_type_common_model "github.com/Rfluid/whatsapp-cloud-api/src/message/model/content-type/common"
 )
 
 // Described at
@@ -11,9 +10,9 @@ import (
 type Header struct {
 	Type HeaderType `json:"type"` // Type of message.
 
-	Text     *message_type_common_model.TextData `json:"text,omitempty"`
-	Image    *media_model.UseMedia               `json:"image,omitempty"`
-	Video    *media_model.UseMedia               `json:"video,omitempty"`
-	Document *media_model.UseMedia               `json:"document,omitempty"`
-	Audio    *media_model.UseMedia               `json:"audio,omitempty"`
+	Text     *string               `json:"text,omitempty"`
+	Image    *media_model.UseMedia `json:"image,omitempty"`
+	Video    *media_model.UseMedia `json:"video,omitempty"`
+	Document *media_model.UseMedia `json:"document,omitempty"`
+	Audio    *media_model.UseMedia `json:"audio,omitempty"`
 }
