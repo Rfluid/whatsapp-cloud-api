979d17b7fd55af1347d6b60e057085fe235ca70d
Rfluid <ruy.vieiraneto@gmail.com>
2024-09-19 23:08:15 -0300

    Add button type at template

--- src/template/model/parameter.model.go
+++ src/template/model/parameter.model.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Parameter struct {
-	Type     ParameterType                       `json:"type"`
-	Text     string                              `json:"text,omitempty"`
-	Image    *media_model.UseMedia               `json:"image,omitempty"`
-	Video    *media_model.UseMedia               `json:"video,omitempty"`
-	Document *media_model.UseMedia               `json:"document,omitempty"`
-	Audio    *media_model.UseMedia               `json:"audio,omitempty"`
-	Sticker  *media_model.UseMedia               `json:"sticker,omitempty"`
-	DateTime *message_type_common_model.DateTime `json:"date_time,omitempty"`
-	Currency *message_type_common_model.Currency `json:"currency,omitempty"`
-	message_type_common_model.ButtonData
+	Type     ParameterType                         `json:"type"`
+	Text     string                                `json:"text,omitempty"`
+	Image    *media_model.UseMedia                 `json:"image,omitempty"`
+	Video    *media_model.UseMedia                 `json:"video,omitempty"`
+	Document *media_model.UseMedia                 `json:"document,omitempty"`
+	Audio    *media_model.UseMedia                 `json:"audio,omitempty"`
+	Sticker  *media_model.UseMedia                 `json:"sticker,omitempty"`
+	DateTime *message_type_common_model.DateTime   `json:"date_time,omitempty"`
+	Currency *message_type_common_model.Currency   `json:"currency,omitempty"`
+	Button   *message_type_common_model.ButtonData `json:"button,omitempty"`
 }
