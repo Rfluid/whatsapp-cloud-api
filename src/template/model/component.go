c45104842acd2ca470afbe5377f57952c2d54f8e
Ruy Vieira <ruy.vieiraneto@gmail.com>
2025-07-12 16:01:16 -0300

    fix: validation strategies

--- src/template/model/component.go
+++ src/template/model/component.go
@@ -6,6 +6,6 @@ type Component struct {
 	Type       ComponentType `json:"type" validate:"required,template_component_type"`
 	Parameters []Parameter   `json:"parameters"`
 
-	SubType message_type_common_model.ButtonSubType `json:"sub_type,omitempty" validate:"template_button_sub_type"` // Only for button type.
-	Index   string                                  `json:"index,omitempty"`                                        // Only for button type. This is actually an integer string.
+	SubType message_type_common_model.ButtonSubType `json:"sub_type,omitempty" validate:"omitempty,template_button_sub_type"` // Only for button type.
+	Index   string                                  `json:"index,omitempty"`                                                  // Only for button type. This is actually an integer string.
 }
