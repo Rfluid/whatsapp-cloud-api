5d8061c15747d476952286dbb014689e181ff2c8
Rfluid <ruy.vieiraneto@gmail.com>
2025-11-17 13:57:33 -0300

    feature: new template categories

--- src/template/model/template-category.go
+++ src/template/model/template-category.go
@@ -3,15 +3,45 @@ package template_model
 type TemplateCategory string
 
 const (
-	Authentication TemplateCategory = "AUTHENTICATION"
-	Marketing      TemplateCategory = "MARKETING"
-	Utility        TemplateCategory = "UTILITY"
+	AccountUpdate         TemplateCategory = "ACCOUNT_UPDATE"
+	PaymentUpdate         TemplateCategory = "PAYMENT_UPDATE"
+	PersonalFinanceUpdate TemplateCategory = "PERSONAL_FINANCE_UPDATE"
+	ShippingUpdate        TemplateCategory = "SHIPPING_UPDATE"
+	ReservationUpdate     TemplateCategory = "RESERVATION_UPDATE"
+	IssueResolution       TemplateCategory = "ISSUE_RESOLUTION"
+	AppointmentUpdate     TemplateCategory = "APPOINTMENT_UPDATE"
+	TransportationUpdate  TemplateCategory = "TRANSPORTATION_UPDATE"
+	TicketUpdate          TemplateCategory = "TICKET_UPDATE"
+	AlertUpdate           TemplateCategory = "ALERT_UPDATE"
+	AutoReply             TemplateCategory = "AUTO_REPLY"
+	Transactional         TemplateCategory = "TRANSACTIONAL"
+	OTP                   TemplateCategory = "OTP"
+	Utility               TemplateCategory = "UTILITY"
+	Marketing             TemplateCategory = "MARKETING"
+	Authentication        TemplateCategory = "AUTHENTICATION"
+	FreeService           TemplateCategory = "FREE_SERVICE"
 )
 
-// IsValid checks if the category is one of the predefined ones.
+// IsValid checks if the category is one of the predefined values.
 func (tc TemplateCategory) IsValid() bool {
 	switch tc {
-	case Authentication, Marketing, Utility:
+	case AccountUpdate,
+		PaymentUpdate,
+		PersonalFinanceUpdate,
+		ShippingUpdate,
+		ReservationUpdate,
+		IssueResolution,
+		AppointmentUpdate,
+		TransportationUpdate,
+		TicketUpdate,
+		AlertUpdate,
+		AutoReply,
+		Transactional,
+		OTP,
+		Utility,
+		Marketing,
+		Authentication,
+		FreeService:
 		return true
 	default:
 		return false
